19a9c521ff802e8656d1808e687ab42e05bb5813
sun-dazer <147622828+sun-dazer@users.noreply.github.com>
2024-04-19 11:46:54 -0400

    Working SFML test

--- display.cpp
+++ display.cpp
@@ -1,9 +1,26 @@
 #include <SFML/Graphics.hpp>
+#include <functional>
 #include <vector>
 #include <string>
 #include <iostream>
 
 int main(){
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-    
+    sf::Font font;
+    font.loadFromFile("Cave-Story.ttf");
+    sf::Text text("hey", font);
+    text.setFillColor(sf::Color::White);
+
+    while(window.isOpen()) {
+        sf::Event e;
+
+        while(window.pollEvent(e)){
+            if(e.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(text);
+        window.display();
+    }
 }
--- main.cpp
+++ main.cpp
@@ -6,21 +6,21 @@
 using std::string;
 using std::endl;
 
-int main() {
-    /* read file in */
-    std::ifstream data("testData.csv");
-
-    // check if file opened successfully
-    if (!data.is_open()) {
-        std::cerr << "Error opening test file.\n";
-    }
-    
-    // print out line by line
-    string line;
-    while (std::getline(data, line)) {
-        std::cout << line << endl;
-    }
-    data.close();
-
-    return 0;
-}
+//int main() {
+//    /* read file in */
+//    std::ifstream data("testData.csv");
+//
+//    // check if file opened successfully
+//    if (!data.is_open()) {
+//        std::cerr << "Error opening test file.\n";
+//    }
+//
+//    // print out line by line
+//    string line;
+//    while (std::getline(data, line)) {
+//        std::cout << line << endl;
+//    }
+//    data.close();
+//
+//    return 0;
+//}
